455add1488814135a96d5ee63ab0dcc872bd7897
dakoine <104214704+MrObfuscator@users.noreply.github.com>
2022-05-05 19:38:15 +0800

    yep

---
+++ games.lua
@@ -0,0 +1,29 @@
+local gid = game.PlaceId
+
+if gid == 286090429 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrObfuscator/FuckingSlavesWith300Bucks/main/Games/ARS.lua"))() -- arsenal
+end
+if gid == 6284583030 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrObfuscator/FuckingSlavesWith300Bucks/main/Games/PSX.lua"))() -- pet sim x
+end
+if gid == 7560156054 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrObfuscator/FuckingSlavesWith300Bucks/main/Games/CLS.lua"))() -- clicker simulator
+end
+if gid == 8540346411 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrObfuscator/FuckingSlavesWith300Bucks/main/Games/RBX.lua"))() -- rebirth champions simulator
+end
+if gid == 7788040584 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrObfuscator/FuckingSlavesWith300Bucks/main/Games/PL.lua"))() -- pls donate me
+end
+if gid == 3101667897 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrObfuscator/FuckingSlavesWith300Bucks/main/Games/LFS.lua"))() -- legends of speed
+end
+if gid == 9300407930 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrObfuscator/FuckingSlavesWith300Bucks/main/Games/BT.lua"))() -- bot clash
+end
+if gid == 6769764667 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrObfuscator/FuckingSlavesWith300Bucks/main/Games/FT.lua"))() -- factory simulator
+end
+if gid == 6769764667 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/MrObfuscator/FuckingSlavesWith300Bucks/main/Games/MS2.lua"))() -- magnet sim 2
+end
